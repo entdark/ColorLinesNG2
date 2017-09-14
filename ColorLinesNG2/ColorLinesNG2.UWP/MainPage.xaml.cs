df8374d2c6049484e9ae1c6e2b910511db79915a
ent <entdark@mail.ru>
2017-09-14 21:31:47 +0500

    UWP desktop title bar is now dark

--- ColorLinesNG2/ColorLinesNG2.UWP/MainPage.xaml.cs
+++ ColorLinesNG2/ColorLinesNG2.UWP/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
+using Windows.UI;
+using Windows.ApplicationModel.Core;
 
 namespace ColorLinesNG2.UWP {
 	public sealed partial class MainPage : IAudioManagerControl {
@@ -15,6 +17,7 @@ namespace ColorLinesNG2.UWP {
 				ApplicationView.PreferredLaunchViewSize = size;
 				ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 				ApplicationView.GetForCurrentView().SetPreferredMinSize(size);
+//				bool isFullScreenLast = false;
 				this.SizeChanged += (sender, ev) => {
 					var newSize = ev.NewSize;
 					bool update = false;
@@ -27,16 +30,65 @@ namespace ColorLinesNG2.UWP {
 						update = true;
 						height = size.Height;
 					}
+					var view = ApplicationView.GetForCurrentView();
 					if (update)
-						ApplicationView.GetForCurrentView().TryResizeView(new Windows.Foundation.Size(width, height));
+						view.TryResizeView(new Windows.Foundation.Size(width, height));
+					bool isFullScreen = view.AdjacentToLeftDisplayEdge && view.AdjacentToRightDisplayEdge;
+//					if (isFullScreen != isFullScreenLast)
+//						this.SetUpTitleBar(isFullScreen);
+//					isFullScreenLast = isFullScreen;
 				};
+				this.SetUpTitleBar(false);
 			}
 
-			LoadApplication(new ColorLinesNG2.App());
+			this.LoadApplication(new ColorLinesNG2.App());
 
 			this.Control = this.Content as Canvas;
 			if (this.Control == null)
 				this.Control = new Canvas();
+		}
+
+		private void SetUpTitleBar(bool isFullScreen) {
+//			if (isFullScreen)
+//				ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
+			isFullScreen = false;
+			var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+			coreTitleBar.ExtendViewIntoTitleBar = isFullScreen;
+			var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+			titleBar.BackgroundColor =
+			titleBar.ButtonBackgroundColor = isFullScreen ? Colors.Transparent : Colors.Black;
+			titleBar.ForegroundColor =
+			titleBar.ButtonForegroundColor = new Windows.UI.Color() {
+				R = 204,
+				G = 204,
+				B = 204,
+				A = 255
+			};
+			titleBar.InactiveForegroundColor =
+			titleBar.ButtonInactiveForegroundColor = new Windows.UI.Color() {
+				R = 136,
+				G = 136,
+				B = 136,
+				A = 255
+			};
+			titleBar.InactiveBackgroundColor =
+			titleBar.ButtonInactiveBackgroundColor =
+			titleBar.ButtonHoverBackgroundColor = isFullScreen ? Colors.Transparent :
+			new Windows.UI.Color() {
+				R = 26,
+				G = 26,
+				B = 26,
+				A = 255
+			};
+			titleBar.ButtonPressedBackgroundColor = isFullScreen ? Colors.Transparent :
+			new Windows.UI.Color() {
+				R = 57,
+				G = 57,
+				B = 57,
+				A = 255
+			};
+			titleBar.ButtonPressedForegroundColor =
+			titleBar.ButtonHoverForegroundColor = Colors.White;
 		}
 	}
 }
