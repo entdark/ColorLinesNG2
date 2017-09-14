3f41652f7276d059d373ad070d20de1705b3319c
ent <entdark@mail.ru>
2017-09-14 21:33:17 +0500

    UWP desktop default window size is now a bit smaller to fit into 768 pixels hight displays

--- ColorLinesNG2/ColorLinesNG2/App.cs
+++ ColorLinesNG2/ColorLinesNG2/App.cs
@@ -4,8 +4,8 @@ using Xamarin.Forms;
 
 namespace ColorLinesNG2 {
 	public class App : Application {
-		public const double MinDesktopWidth = 420.0;
-		public const double MinDesktopHeight = 768.0;
+		public const double MinDesktopWidth = 377.0;
+		public const double MinDesktopHeight = 690.0;
 		public const double MinDesktopRatio = 0.546875;
 
 		public static readonly IAudioManager AudioManager;
