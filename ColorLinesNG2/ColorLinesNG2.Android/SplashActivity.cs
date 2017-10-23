f96bf9a5eb113c15d2ad6b9e76265245b55c611b
ent <entdark@mail.ru>
2017-10-24 01:02:01 +0500

    Android speeded up loading the splash screen a bit

--- ColorLinesNG2/ColorLinesNG2.Android/SplashActivity.cs
+++ ColorLinesNG2/ColorLinesNG2.Android/SplashActivity.cs
@@ -1,9 +1,6 @@
-﻿using System.Threading.Tasks;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.OS;
 using Android.Support.V7.App;
 
 namespace ColorLinesNG2.Droid {
@@ -15,16 +12,11 @@ namespace ColorLinesNG2.Droid {
 		ScreenOrientation = ScreenOrientation.Portrait
 	)]
 	public class SplashActivity : AppCompatActivity {
-		public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-			base.OnCreate(savedInstanceState, persistentState);
-		}
-
-		// Launches the startup task
 		protected override void OnResume() {
 			base.OnResume();
-			StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+			this.StartActivity(new Intent(Application.Context, typeof(MainActivity)));
 		}
-		
+
 		//to prevent closing while launching
 		public override void OnBackPressed() {}
 	}
